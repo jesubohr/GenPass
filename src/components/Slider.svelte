c34c130274965a84e7698c0b71527cf6a73d72df
Jesús Borrero <jesufer2018@hotmail.com>
2022-12-10 01:12:38 -0500

    Add Slider component

---
+++ src/components/Slider.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let min = 0
+  export let max = 100
+  export let value = min
+</script>
+
+<div class="flex items-center gap-4 w-full">
+  <input type="range" class="w-full" {min} {max} bind:value />
+  <span class="w-8 h-8 text-center text-2xl font-semibold">{value}</span>
+</div>
