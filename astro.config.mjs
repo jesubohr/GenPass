5018f4adc1cb5b186b33631d4cb0902f893d6230
Jesús Borrero <jesufer2018@hotmail.com>
2022-12-10 02:50:44 -0500

    Fix on Astro url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://altgenpass.vercel.app/',
   integrations: [svelte(), tailwind()]
 })
